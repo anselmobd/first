3adb1282245289d1cd48eea4b110abb1347def13
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-22 16:01:26 -0300

    `UserData` para `PUT` with optional fields

--- model.py
+++ model.py
@@ -1,11 +1,15 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
-class UserIn(BaseModel):
+class User(BaseModel):
+    id: int
     name: str = Field(..., min_length=1, max_length=50)
     email: str = Field(..., min_length=3, max_length=50)
     password: str = Field(..., min_length=3, max_length=50)
 
 
-class User(UserIn):
-    id: int
+class UserData(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=50)
+    email: Optional[str] = Field(None, min_length=3, max_length=50)
+    password: Optional[str] = Field(None, min_length=3, max_length=50)

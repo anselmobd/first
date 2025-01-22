7752169d6929726e0a371fc9af878a6d7103f1aa
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-22 16:00:53 -0300

    add update_user

--- database.py
+++ database.py
@@ -1,13 +1,19 @@
 from pprint import pprint
+from model import User
 
 
 class Users():
     def __init__(self):
-        self.__table = []
+        self._table = []
     
     def add_user(self, user):
-        self.__table.append(user)
+        self._table.append(dict(user))
+
+    def update_user(self, id, updated_user):
+        for user in self._table:
+            if user['id'] == id:
+                user.update(dict(updated_user))
+                return user
 
     def pprint(self):
-        pprint(self.__table)
-        
+        pprint(self._table)
